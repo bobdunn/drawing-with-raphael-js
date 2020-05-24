d764e9d9a0b8b67fd189a2cd665ed785453426c5
Bob Dunn <rpdunn@gmail.com>
2020-05-23 23:07:41 -0600

    add size variation to the circles

--- public/js/mandala.js
+++ public/js/mandala.js
@@ -3,10 +3,11 @@ import * as utils from './utils.js'
 export const drawBasicDotMandala = paper => {
     const pointCount = 28;
     const getR = t => (Math.pow(t, 4) / (2 * Math.pow(Math.PI, 4))) + 1 / 2
+    const getCircleRadius = t => .14 * Math.pow(t / Math.PI, 2) + .01
     for (let theta = -Math.PI; theta < Math.PI; theta += 2 * Math.PI / pointCount) {
         const r = getR(theta);
         console.log(r, theta)
-        utils.polar.drawCircle(paper, { center: { r: r * 0.95, theta: theta - Math.PI / 2 }, radius: 0.05 })
+        utils.polar.drawCircle(paper, { center: { r: r * 0.85, theta: theta - Math.PI / 2 }, radius: getCircleRadius(theta) })
 
     }
 }
